7362c8958a945920c226bf66d2bd5075e907a740
Xerxes Jason Bangcoyo <xerxesjason.bangcoyo@fligno.com>
2024-01-25 20:54:38 +0800

    add loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import { Pagination } from 'react-bootstrap';
+import { Pagination, Spinner } from 'react-bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App = () => {
@@ -10,14 +10,18 @@ const App = () => {
   const [currentPage, setCurrentPage] = useState(1);
   const [itemsPerPage] = useState(7);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
+    setLoading(true)
     const fetchData = async (url, setData) => {
       try {
         const response = await axios.get(url);
         setData(response.data);
       } catch (error) {
         console.error(`Error fetching data from ${url}:`, error);
+      } finally {
+        setLoading(false)
       }
     };
 
@@ -68,162 +72,168 @@ const App = () => {
   };
 
   return (
-    <div className="container">
-      <h1>Tech Stack EOL</h1>
-      <div className="row">
-        <div className="col-md-12">
-          <ul className="nav nav-tabs">
-            <li className="nav-item">
-              <button
-                className={`nav-link ${activeTab === 'php' ? 'active' : ''}`}
-                onClick={() => handleTabChange('php')}
-              >
-                PHP
-              </button>
-            </li>
-            <li className="nav-item">
-              <button
-                className={`nav-link ${activeTab === 'mysql' ? 'active' : ''}`}
-                onClick={() => handleTabChange('mysql')}
-              >
-                MySQL
-              </button>
-            </li>
-          </ul>
-        </div>
+    <>
+    {loading ? (
+      <div className="d-flex align-items-center justify-content-center" style={{ minHeight: '100vh' }}>
+      <Spinner animation="grow" />
+    </div>
+    ) : <div className="container">
+    <h1>Tech Stack EOL</h1>
+    <div className="row">
+      <div className="col-md-12">
+        <ul className="nav nav-tabs">
+          <li className="nav-item">
+            <button
+              className={`nav-link ${activeTab === 'php' ? 'active' : ''}`}
+              onClick={() => handleTabChange('php')}
+            >
+              PHP
+            </button>
+          </li>
+          <li className="nav-item">
+            <button
+              className={`nav-link ${activeTab === 'mysql' ? 'active' : ''}`}
+              onClick={() => handleTabChange('mysql')}
+            >
+              MySQL
+            </button>
+          </li>
+        </ul>
       </div>
-      <div className="row mt-3">
-        <div className="col-md-12">
-          <div className="tab-content">
-            <div className={`tab-pane ${activeTab === 'php' ? 'fade active show' : 'fade'}`}>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search..."
-                  value={searchTerm}
-                  onChange={handleSearch}
-                />
-              </div>
-              {currentPhpInfo?.length > 0 ? (
-                <div>
-                  <table className="table table-bordered table-hover">
-                    <thead className="table-dark">
-                      <tr>
-                        <th>Cycle</th>
-                        <th>Latest Version</th>
-                        <th>Release Date</th>
-                        <th>Latest Release Date</th>
-                        <th>End of Life Date</th>
-                        <th>Support Date</th>
-                        <th>LTS</th>
+    </div>
+    <div className="row mt-3">
+      <div className="col-md-12">
+        <div className="tab-content">
+          <div className={`tab-pane ${activeTab === 'php' ? 'fade active show' : 'fade'}`}>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleSearch}
+              />
+            </div>
+            {currentPhpInfo?.length > 0 ? (
+              <div>
+                <table className="table table-bordered table-hover">
+                  <thead className="table-dark">
+                    <tr>
+                      <th>Cycle</th>
+                      <th>Latest Version</th>
+                      <th>Release Date</th>
+                      <th>Latest Release Date</th>
+                      <th>End of Life Date</th>
+                      <th>Support Date</th>
+                      <th>LTS</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                  {currentPhpInfo.map((versionInfo, index) => (
+                      <tr key={index}>
+                        <td>{versionInfo.cycle}</td>
+                        <td>{versionInfo.latest}</td>
+                        <td>{versionInfo.releaseDate}</td>
+                        <td>{versionInfo.latestReleaseDate}</td>
+                        <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.eol) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.eol}</td>
+                        <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.support) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.support}</td>
+                        <td>{versionInfo.lts.toString()}</td>
                       </tr>
-                    </thead>
-                    <tbody>
-                    {currentPhpInfo.map((versionInfo, index) => (
-                        <tr key={index}>
-                          <td>{versionInfo.cycle}</td>
-                          <td>{versionInfo.latest}</td>
-                          <td>{versionInfo.releaseDate}</td>
-                          <td>{versionInfo.latestReleaseDate}</td>
-                          <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.eol) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.eol}</td>
-                          <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.support) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.support}</td>
-                          <td>{versionInfo.lts.toString()}</td>
-                        </tr>
-                      ))}
+                    ))}
 
-                    </tbody>
-                  </table>
-                  <Pagination className="d-flex justify-content-end">
-                    <Pagination.First onClick={() => paginate(1)} />
-                    <Pagination.Prev onClick={() => paginate(currentPage - 1)} />
-                    {Array.from({ length: Math.ceil(allPhpInfo.length / itemsPerPage) }, (_, index) => (
-                      <Pagination.Item
-                        key={index + 1}
-                        active={currentPage === index + 1}
-                        onClick={() => paginate(index + 1)}
-                      >
-                        {index + 1}
-                      </Pagination.Item>
-                    ))}
-                    <Pagination.Next onClick={() => paginate(currentPage + 1)} />
-                    <Pagination.Last
-                      onClick={() => paginate(Math.ceil(allPhpInfo.length / itemsPerPage))}
-                    />
-                  </Pagination>
-                </div>
-              ) : (
-                <div className="text-center">
-                  <p>No results found for your search.</p>
-                </div>
-              )}
-            </div>
-            <div className={`tab-pane ${activeTab === 'mysql' ? 'fade active show' : 'fade'}`}>
-              <div className="mb-3">
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Search..."
-                  value={searchTerm}
-                  onChange={handleSearch}
-                />
+                  </tbody>
+                </table>
+                <Pagination className="d-flex justify-content-end">
+                  <Pagination.First onClick={() => paginate(1)} />
+                  <Pagination.Prev onClick={() => paginate(currentPage - 1)} />
+                  {Array.from({ length: Math.ceil(allPhpInfo.length / itemsPerPage) }, (_, index) => (
+                    <Pagination.Item
+                      key={index + 1}
+                      active={currentPage === index + 1}
+                      onClick={() => paginate(index + 1)}
+                    >
+                      {index + 1}
+                    </Pagination.Item>
+                  ))}
+                  <Pagination.Next onClick={() => paginate(currentPage + 1)} />
+                  <Pagination.Last
+                    onClick={() => paginate(Math.ceil(allPhpInfo.length / itemsPerPage))}
+                  />
+                </Pagination>
               </div>
-              {currentMysqlInfo?.length > 0 ? (
-                <div>
-                  <table className="table table-bordered table-hover">
-                    <thead className="table-dark">
-                      <tr>
-                        <th>Cycle</th>
-                        <th>Latest Version</th>
-                        <th>Release Date</th>
-                        <th>Latest Release Date</th>
-                        <th>End of Life Date</th>
-                        <th>Support Date</th>
-                        <th>LTS</th>
-                      </tr>
-                    </thead>
-                    <tbody>
-                      {currentMysqlInfo.map((versionInfo, index) => (
-                        <tr key={index}>
-                          <td>{versionInfo.cycle}</td>
-                          <td>{versionInfo.latest}</td>
-                          <td>{versionInfo.releaseDate}</td>
-                          <td>{versionInfo.latestReleaseDate}</td>
-                          <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.eol) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.eol}</td>
-                          <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.support) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.support}</td>
-                          <td>{versionInfo.lts.toString()}</td>
-                        </tr>
-                      ))}
-                    </tbody>
-                  </table>
-                  <Pagination className="d-flex justify-content-end">
-                    <Pagination.First onClick={() => paginate(1)} />
-                    <Pagination.Prev onClick={() => paginate(currentPage - 1)} />
-                    {Array.from({ length: Math.ceil(allMysqlInfo.length / itemsPerPage) }, (_, index) => (
-                      <Pagination.Item
-                        key={index + 1}
-                        active={currentPage === index + 1}
-                        onClick={() => paginate(index + 1)}
-                      >
-                        {index + 1}
-                      </Pagination.Item>
-                    ))}
-                    <Pagination.Next onClick={() => paginate(currentPage + 1)} />
-                    <Pagination.Last
-                      onClick={() => paginate(Math.ceil(allMysqlInfo.length / itemsPerPage))}
-                    />
-                  </Pagination>
-                </div>
-              ) : (
-                <div className="text-center">
-                  <p>No results found for your search.</p>
-                </div>
-              )}
+            ) : (
+              <div className="text-center">
+                <p>No results found for your search.</p>
+              </div>
+            )}
+          </div>
+          <div className={`tab-pane ${activeTab === 'mysql' ? 'fade active show' : 'fade'}`}>
+            <div className="mb-3">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search..."
+                value={searchTerm}
+                onChange={handleSearch}
+              />
             </div>
+            {currentMysqlInfo?.length > 0 ? (
+              <div>
+                <table className="table table-bordered table-hover">
+                  <thead className="table-dark">
+                    <tr>
+                      <th>Cycle</th>
+                      <th>Latest Version</th>
+                      <th>Release Date</th>
+                      <th>Latest Release Date</th>
+                      <th>End of Life Date</th>
+                      <th>Support Date</th>
+                      <th>LTS</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    {currentMysqlInfo.map((versionInfo, index) => (
+                      <tr key={index}>
+                        <td>{versionInfo.cycle}</td>
+                        <td>{versionInfo.latest}</td>
+                        <td>{versionInfo.releaseDate}</td>
+                        <td>{versionInfo.latestReleaseDate}</td>
+                        <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.eol) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.eol}</td>
+                        <td style={{ backgroundColor: isDateBelowCurrentDate(versionInfo.support) ? '#FFCCCB' : '#90ee90' }}>{versionInfo.support}</td>
+                        <td>{versionInfo.lts.toString()}</td>
+                      </tr>
+                    ))}
+                  </tbody>
+                </table>
+                <Pagination className="d-flex justify-content-end">
+                  <Pagination.First onClick={() => paginate(1)} />
+                  <Pagination.Prev onClick={() => paginate(currentPage - 1)} />
+                  {Array.from({ length: Math.ceil(allMysqlInfo.length / itemsPerPage) }, (_, index) => (
+                    <Pagination.Item
+                      key={index + 1}
+                      active={currentPage === index + 1}
+                      onClick={() => paginate(index + 1)}
+                    >
+                      {index + 1}
+                    </Pagination.Item>
+                  ))}
+                  <Pagination.Next onClick={() => paginate(currentPage + 1)} />
+                  <Pagination.Last
+                    onClick={() => paginate(Math.ceil(allMysqlInfo.length / itemsPerPage))}
+                  />
+                </Pagination>
+              </div>
+            ) : (
+              <div className="text-center">
+                <p>No results found for your search.</p>
+              </div>
+            )}
           </div>
         </div>
       </div>
     </div>
+  </div>}
+    </>
   );
 };
 
